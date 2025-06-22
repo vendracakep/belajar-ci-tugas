ac6c2f1756594223b224bb230792e388edb30dca
vendracakep <111202314876@mhs.dinus.ac.id>
2025-06-22 22:33:24 +0700

    WebService

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -28,6 +28,13 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('clear', 'TransaksiController::cart_clear');
 });
 
+
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
+
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 $routes->get('faq', 'FaqController::index', ['filter' => 'auth']);
 $routes->get('contact', 'KontakController::index', ['filter' => 'auth']);
