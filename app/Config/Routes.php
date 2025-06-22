0f69e64b7f533a1e502a5165587350ab324a6341
vendracakep <111202314876@mhs.dinus.ac.id>
2025-06-22 23:09:10 +0700

    menambahkan api data transaksi pembelian - WebService Server

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,6 +35,9 @@ $routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
 $routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
 $routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
 
+$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 $routes->get('faq', 'FaqController::index', ['filter' => 'auth']);
 $routes->get('contact', 'KontakController::index', ['filter' => 'auth']);
+
+$routes->resource('api', ['controller' => 'apiController']);
